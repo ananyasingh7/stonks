afe8d32db79a98ca2e85df7d2793e349c31a8cc9
Ananya Singh <as2863@njit.edu>
2020-01-05 02:37:56 -0500

    finish NASDAQ

--- script.sh
+++ script.sh
@@ -1,5 +1,6 @@
-echo "Please enter the stock symbol: "
-read symbol
-wget -O - http://www.eoddata.com/stockquote/NASDAQ/$symbol.htm > now.html
-java -jar tagsoup-1.2.1.jar --files now.html
-python3 scraper.py now.xhtml
+#!/bin/bash
+
+echo "Please type in the stock symbol: "
+read input
+./data.sh $input
+
